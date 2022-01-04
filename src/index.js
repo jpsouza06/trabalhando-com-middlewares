21c6ccd87fb2a8ab3f89ade01472e8a600929630
Joao Paulo <joaopaulosc06@gmail.com>
2022-01-04 13:48:27 -0300

    completed middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,70 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  request.user = user;
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const todosCount = user.todos.reduce((acc) => {
+    return acc + 1
+  }, 0)
+  if(user.pro === true) {
+    return next()
+  } else if(user.pro === false && todosCount < 10) {
+    return next()
+  } else {
+    return response.status(403).json({ message: 'You need to be por to create more todo'})
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params
+  
+  
+  if(!validate(id)) {
+    return response.status(400).json({ error: 'Todo Id is not valid' })
+  }
+
+  const user = users.find(user => user.username === username)
+  if (!user) {
+    return response.status(404).json({ error: 'User not exists' })
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not exists' })
+  }
+  
+  request.user = user
+  request.todo = todo
+  
+  
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find( (user) => user.id === id)
+
+  if(!user) {
+    return response.status(404).json({ error: 'User not found'})
+  }
+
+  request.user = user;
+  return next()
 }
 
 app.post('/users', (request, response) => {
